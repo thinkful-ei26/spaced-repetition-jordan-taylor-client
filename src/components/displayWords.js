e2354ef0b276fed7e3737b7b337ff17573ea0091
taylormerrill <taylormerrill92@gmail.com>
2019-01-30 12:00:10 -0700

    to work on display words

--- src/components/displayWords.js
+++ src/components/displayWords.js
@@ -4,24 +4,24 @@ import {connect} from 'react-redux';
 class DisplayWords extends React.Component {
 
     render() {
-        const questionCard = () => {
-            console.log(this.props);
-            if(this.props.questions.length !== 0){
-              const resultArray =  this.props.questions.map(object => {
-                    console.log('object we are mapping over:', object.question);
-                    return <div>{object.question}</div>
-                }
-                );
-                return resultArray;
-            }
-            else {
-                return (<div>pulling in the knowledge you seek...</div>)
-            }
-        }
+        // const questionCard = () => {
+        //     console.log(this.props);
+        //     if(this.props.questions.length !== 0){
+        //       const resultArray =  this.props.questions.map(object => {
+        //             console.log('object we are mapping over:', object.question);
+        //             return <div>{object.question}</div>
+        //         }
+        //         );
+        //         return resultArray;
+        //     }
+        //     else {
+        //         return (<div>pulling in the knowledge you seek...</div>)
+        //     }
+        // }
         console.log(this.props);
         return (
             <div>
-                {questionCard()}
+                {/* {questionCard()} */}
             </div>
         )
     }
